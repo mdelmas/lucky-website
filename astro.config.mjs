7278f225394c085af24cae6af99151478b94b694
Morgane Delmas <mdelmas93@gmail.com>
2026-01-21 15:21:04 +1000

    [refacto] adding aliases for different file folders

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@sections': path.resolve('./src/components/sections'),
+        '@ui': path.resolve('./src/components/ui'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@assets': path.resolve('./src/assets'),
+        '@styles': path.resolve('./src/styles'),
+      },
+    },
+  },
+});
